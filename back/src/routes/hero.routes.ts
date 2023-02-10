2c6295ea26e835465a922a6adc1bb2dda5af01d2
ludovic-str <ludovic.sutter@epitech.eu>
2023-02-10 20:39:29 +0100

    refacto: hero route return random one

--- back/src/routes/hero.routes.ts
+++ back/src/routes/hero.routes.ts
@@ -6,37 +6,13 @@ import { FastifyPluginDoneFunction } from "../types/global.types";
 import HerosConfig from "../../assets/heros.json";
 import ClientError from "../error";
 
-type IdParams = FastifyRequest<{
-  Params: {
-    id: string;
-  };
-}>;
-
 export default (
   instance: FastifyInstance,
   _opts: FastifyPluginOptions,
   done: FastifyPluginDoneFunction
 ): void => {
-  instance.get("/:id", async (req: IdParams, res: FastifyReply) => {
-    if (isNaN(parseInt(req.params.id))) {
-      throw new ClientError({
-        name: "Invalid Arguments",
-        message: "id param is not an integer",
-        level: "warm",
-        status: httpStatus.BAD_REQUEST,
-      });
-    }
-
-    const id = parseInt(req.params.id);
-
-    if (HerosConfig[id] === undefined) {
-      throw new ClientError({
-        name: "Invalid Arguments",
-        message: "hero index out of range",
-        level: "warm",
-        status: httpStatus.BAD_REQUEST,
-      });
-    }
+  instance.get("/", async (req: FastifyRequest, res: FastifyReply) => {
+    const id = Math.floor(Math.random() * HerosConfig.length);
 
     res.status(httpStatus.OK).send(HerosConfig[id]);
   });
