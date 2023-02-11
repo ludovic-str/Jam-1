adaa4ec91db7c39eccd01d93bd0bd792ad3c94a2
Maxime Hoffbeck <maxime.hoffbeck@epitech.eu>
2023-02-11 22:01:18 +0100

    fix: import react

--- front/src/screens/guessCharacter/index.tsx
+++ front/src/screens/guessCharacter/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { TextField, Button, Autocomplete } from "@mui/material";
 import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
