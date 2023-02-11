bfab25f43b3c35573a3ef8685bdeed5643225e63
ludovic-str <ludovic.sutter@epitech.eu>
2023-02-11 23:58:43 +0100

    feat: easter egg

---
+++ front/src/components/EasterEgg/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useMemo, useState } from "react";
+
+import BatmanGif from "../../assets/batman.gif";
+import "./styles.css";
+
+const EasterEgg = () => {
+  const [easterEggClass, setEasterEggClass] = useState("easter-egg");
+  const pattern = useMemo(() => {
+    const pattern = [
+      "ArrowUp",
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+      "ArrowLeft",
+      "ArrowRight",
+      "b",
+      "a",
+    ];
+    return pattern;
+  }, []);
+
+  useEffect(() => {
+    let index = 0;
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === pattern[index]) {
+        index++;
+        if (index === pattern.length) {
+          setEasterEggClass("easter-egg easter-egg-animation");
+        }
+      } else {
+        index = 0;
+      }
+    };
+    window.addEventListener("keydown", handler);
+    return () => window.removeEventListener("keydown", handler);
+  }, [pattern]);
+
+  return (
+    <>
+      <img src={BatmanGif} alt="batman" className={easterEggClass}></img>
+    </>
+  );
+};
+
+export default EasterEgg;
--- front/src/screens/home/index.tsx
+++ front/src/screens/home/index.tsx
@@ -4,14 +4,18 @@ import Buttons from "../../components/buttons";
 import Background from "../../components/background";
 import Checkboxes from "../../components/checkbox";
 import React from "react";
+import EasterEgg from "../../components/EasterEgg";
 
 const Home = () => {
-  return <div>
-    <Title></Title>
-    <Buttons></Buttons>
-    <Checkboxes></Checkboxes>
-    <Background></Background>
-  </div>;
+  return (
+    <div>
+      <EasterEgg />
+      <Title></Title>
+      <Buttons></Buttons>
+      <Checkboxes></Checkboxes>
+      <Background></Background>
+    </div>
+  );
 };
 
 export default Home;
