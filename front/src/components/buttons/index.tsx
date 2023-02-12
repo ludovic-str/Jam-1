5ad62c3488932a2881e458a9b4efc5e5e9a8ba5a
ludovic-str <ludovic.sutter@epitech.eu>
2023-02-12 15:03:02 +0100

    fix: change github icon by edu

--- front/src/components/buttons/index.tsx
+++ front/src/components/buttons/index.tsx
@@ -1,9 +1,8 @@
-
 import "./styles.css";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import ImageSearchIcon from "@mui/icons-material/ImageSearch";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SchoolIcon from "@mui/icons-material/School";
 import { useNavigate } from "react-router-dom";
 import React from "react";
 
@@ -25,50 +24,65 @@ const ButtonsReverse = () => {
   return (
     <div className="flexbox-container">
       <div className="flexbox-item image_button">
-      <Button variant="contained"
-      style={{maxWidth: '200px',
-      maxHeight: '100px',
-      minWidth: '200px',
-      minHeight: '100px',
-      fontSize: '20px',
-      backgroundColor: "#E33E39",}}
-      onClick={handleImageClick}
-      >
-        <p className="icon"><ImageSearchIcon></ImageSearchIcon></p>
-        <p className="text">Image</p>
-      </Button>
+        <Button
+          variant="contained"
+          style={{
+            maxWidth: "200px",
+            maxHeight: "100px",
+            minWidth: "200px",
+            minHeight: "100px",
+            fontSize: "20px",
+            backgroundColor: "#E33E39",
+          }}
+          onClick={handleImageClick}
+        >
+          <p className="icon">
+            <ImageSearchIcon></ImageSearchIcon>
+          </p>
+          <p className="text">Image</p>
+        </Button>
       </div>
 
       <div className="flexbox-item quote_button">
-        <Button variant="contained"
-      style={{maxWidth: '200px',
-      maxHeight: '100px',
-      minWidth: '200px',
-      minHeight: '100px',
-      fontSize: '20px',
-      backgroundColor: "#E33E39",}}
-      onClick={handleQuoteClick}
-      >
-        <p className="icon"><AccountCircleIcon></AccountCircleIcon></p>
-        <p className="text">Character</p>
-          </Button>
+        <Button
+          variant="contained"
+          style={{
+            maxWidth: "200px",
+            maxHeight: "100px",
+            minWidth: "200px",
+            minHeight: "100px",
+            fontSize: "20px",
+            backgroundColor: "#E33E39",
+          }}
+          onClick={handleQuoteClick}
+        >
+          <p className="icon">
+            <AccountCircleIcon></AccountCircleIcon>
+          </p>
+          <p className="text">Character</p>
+        </Button>
       </div>
       <div className="flexbox-item emoji_button">
-        <Button variant="contained"
-      style={{maxWidth: '200px',
-      maxHeight: '100px',
-      minWidth: '200px',
-      minHeight: '100px',
-      fontSize: '20px',
-      backgroundColor: "#E33E39",}}
-      onClick={handleEmojiClick}
-      >
-        <p className="icon"><GitHubIcon></GitHubIcon></p>
-        <p className="text">Pedago</p>
-          </Button>
+        <Button
+          variant="contained"
+          style={{
+            maxWidth: "200px",
+            maxHeight: "100px",
+            minWidth: "200px",
+            minHeight: "100px",
+            fontSize: "20px",
+            backgroundColor: "#E33E39",
+          }}
+          onClick={handleEmojiClick}
+        >
+          <p className="icon">
+            <SchoolIcon></SchoolIcon>
+          </p>
+          <p className="text">Pedago</p>
+        </Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ButtonsReverse;
